ac0413242a9f34b1b7310f3604cf148500fd7e42
BatimusPrime <primebatimus@gmail.com>
2019-07-27 22:08:29 -0700

    integrating firebase

--- 7_Trivia_Time/sketch.js
+++ 7_Trivia_Time/sketch.js
@@ -1,167 +1,204 @@
+//get HTML DOM elements
+let message = document.getElementById('message');
+let scoreDisp = document.getElementById('score');
+let questText = document.getElementById('questText');
+let index = 0;
+function setup(){
+
+
+    //centers the canvas using p5 variables
+    let cnv = createCanvas(600, 100);
+    let x = (windowWidth - width) / 2;
+    cnv.position(x, windowHeight-(height+100));
+  
+
+//create new user object, arg will come from create element
+user  = new User('Default');
+
+//create new game object, used for game status events
+quest = new Question(1,'Question text goes here',['answer1','answer2','answer3','answer4'],'d')
+game = new Game();
+
+};
+//end setup
+
+
+/////////////////////////////////
+function draw(){
+
+//background color
+background (220);
+
+//show the game piece
+user.show();
+
+};
+
+
+//Question object properties
+class Question{
+
+    constructor(index,text,answers,corr){
+
+        //question index, will be added to 'used list' after displayed so it cannot be re-used
+        this.index = index;
+        //text of question from database
+        this.text = text;
+        //array of answers from database
+        this.answers = answers;
+        //correct answer [a-d]
+        this.corr = corr;
+
+    }
+    //answer function
+    answer(ansId){
+        if(game.status == true){
+            message.innerHTML = 'Game is go! </br>' + user.userName;
+
+        }else{
+        //wrong answer
+        
+        message.innerHTML = 'Game is not go! </br>' + user.userName;
+    }
+
+
+}
+
+    show(){
+        this.status = true;
+        //display the question text with an index # bullet
+        questText.innerHTML = ('Question #' + this.index + ': ' + this.text);
+        
+        //for loop to get and display all 4 answer containers
+        for (let i=0;i<4;i++){
+            let ansContainer = document.getElementById('ans' + i);
+            ansContainer.innerHTML = this.answers[i];
+        }
+
+    }
+    next(){
 /*
-
-Need to check for end of array and display new question text 
-
-i.e. if next step in array is empty display "no more fun"
-
-
-BIG PROBLEM ARRAY STARTS AT -1 WHY WHY WHY
+new question object, keeps questions under 10 otherwise question number keeps scrolling
+eventually this will be a random index pulled from DB and the test will be uneeded
 
 */
-
-
-
-//var chars = [andy,angela,creed,darryl,dwight,jim,kelly,kevin,meredith,michael,oscar,pam,ryan,stanley]
-//initialize character options
-var andy;
-var angela;
-var creed;
-
-//initialize score
-var score = 1;
-
-//generic vars - refactor later
-var a,b,c,d,q;
-
-//question iterator
-var i = 0;
-
-//question display number
-var j = 1;
-
-//question and answer ident
-var corAns = 1;
-
-//loads images
-function preload(){
-    
-    andy = loadImage('img/andy.png') ;
-    angela = loadImage('img/angela.png');
-    creed = loadImage('img/creed.png');
-//    darryl = loadImage('')
-//    dwight = loadImage('')
-//    jim = loadImage('')
-//    kelly = loadImage('')
-//    kevin = loadImage('')
-//    meredith = loadImage('')
-//    michael = loadImage('')
-//    oscar = loadImage('')
-//    pam = loadImage('')
-//    ryan = loadImage('')        
-//    stanley = loadImage('')
-//    
-}
-
-
-function setup(){
-    //create canvas
-    var canvas = createCanvas(400,400);
-    
-    //in the main div
-    canvas.parent('main');
-    
-    //create new player, set icon and name
-    player = new Player(creed,"Stephen",0);
-    
-    //display new question
-    question = new Question();
-    
-    
-}
-
-function draw(){
-    
-    //fill canvas
-    background(51);
-    
-    //display player icon and name
-    player.show();
-    
-    //display question
-    question.show(i);
-    
-    //display score
-    player.update(0);
-
-}
-
-//player constructor
-function Player(icon, name, score){
-
-    //player's name
-    this.name = name;
-    
-    //duisplay icon and name
-    this.show = function(){
-           
-        //image and text options
-        image(icon,25,50);
+        if(quest.index < 10){
+        quest = new Question(this.index + 1,'Question text goes here',['answer1','answer2','answer3','answer4'],'d');
         
-        textSize(32);
-        text(name, 10, 30);
-        fill(255, 255, 255); 
-        
+        //display question and answer text
+        this.show();
+    }else{}
 
-    };
-    
-
-    
-    this.update = function(score){
-
-        //score
-        textSize(32);
-        text(score, 300, 30);
-        fill(255, 255, 255); 
-    
-    };
 
 }
 
-//question constructor
-function Question(){
 
-    //display question
-    this.show = function(i){
-    
-    //get question from array (a) in push.js 
-    //j is question number, i is index position in array. (j = i+1) should be true
-    q = ("Question " + j + ": " + a[i].q);
-    
-    //text options
-    textSize(18);
-    text(q,10,200,400,300);
-   
+};
+  //User object properties
+  class User{
 
+    constructor(userName){
+        //7 provides a margin around the border, initial position
+        this.x = 7;
+        this.y = 7;
+        this.game = true;
+
+        //set score to 0;
+        this.score = 0;
+
+        //user name
+        this.userName = userName;
     }
-    
-    //next question function
-    this.next = function(ans){
 
-        index = a.indexOf(ans);
-        console.log(index);
+    //display the user's game piece
+    show(){
         
+        //display the user's piece
+        rect(this.x, this.y, 50,80);
 
-//        if(i<a.length-1 && ans == a[i].a){
-//            j++;
-//            i++;
-//            player.update(score++);
-//        }else{
-//            console.log("end of array");
-//        
-//        }
-    };
-    
-    //process answer
-    this.answer = function(ans){
-        
-        console.log("Question: " + (j) + "/" + a.length + " Answer Submitted: " + ans + " Correct Answer: " + a[i].a + " Array Index: " + i + " Score: " + (score));
-        
-                        
-        question.next(ans);
-                    
-            
-        
-    
-    }
-}
   
+     
+ 
+
+    }
+    
+    //move the piece after a score, this is a correct answer
+    move(){
+    
+    //display status message
+    message.innerHTML = 'Correct </br> ' + this.userName;
+
+
+    //keep score under 10
+    if(this.score < 10){
+    //increment score
+    this.score++;
+        //move the piece on the x axis
+        this.x += 53;
+        scoreDisp.innerHTML = this.score;
+
+        //check if avatar is off canvas
+    }else if (this.score == 10 && this.x < 350){
+
+            message.innerHTML = user.userName + ' </br> WINS!!';
+            this.x += 53;
+        }else{
+
+
+        }
+        //next question
+        quest.next();
+        
+    }
+    
+    //set the piece back to 0
+    reset(){
+
+        //reset status message
+        message.innerHTML = 'Message: UserName';
+        //original coordinate
+        this.x = 7;
+        //zero score
+        this.score = 0;
+        //display zero score
+        scoreDisp.innerHTML = 0;
+        //reset answers
+        for (let j=0;j<4;j++){
+
+            let ansClear = document.getElementById('ans' + j);
+            ansClear.innerHTML = ''
+
+        }
+        questText.innerHTML = ''
+
+        //turn game status to off
+        quest.status = false;
+    }
+
+    wrong(){
+        
+        message.innerHTML = 'Wrong </br>' + user.userName;
+    }
+ 
+  }//end of user
+
+class Game{
+
+    constructor(){
+
+        this.status = true;
+
+    }
+    go(){
+
+     //create new question object
+    let quest = new Question(1,'Question text goes here',['answer1','answer2','answer3','answer4'],'d');
+    //display user name and initial message
+    message.innerHTML = 'Let\'s Go! </br>' + user.userName;    
+    //display score
+    scoreDisp.innerHTML = user.score;
+    //display question text
+    quest.show();
+
+    }
+}
---
+++ 8_NRCS_API/js/config.js
@@ -0,0 +1,20 @@
+{
+    'API_KEY' : 'AIzaSyAkDwyY4u8FyGkqr7B4K3bGnd9hy4Jwjao',
+    'PROJECT_ID' : 'nrcsgraphs',
+    'APP_ID' : '1:946223386871:web:6f8e0aab92f84b2c'
+}
+const firebaseConfig = {
+        
+    apiKey: API_KEY,
+    
+    authDomain: PROJECT_ID + '.firebaseapp.com',
+    
+    databaseURL: 'https://' + PROJECT_ID + '.firebaseio.com',
+    
+    projectId: PROJECT_ID,
+    
+    storageBucket: PROJECT_ID + '.appspot.com',
+        
+    appID: APP_ID,
+
+};
